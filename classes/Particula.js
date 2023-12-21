27a6ca9e2af2c8dce121b69da9700ed93dbc0e87
mateoagudo <mateo.agudo@ucuenca.edu.ec>
2023-12-21 13:01:27 -0500

    se creó una partícula con un tiempo de vida que se mueve de forma aleatoria hacia arriba, derecha, abajo e izquierda, se creó un sistema de partículas que añade una partícula nueva cada fotograma, se mapeo la posición del sistema por medio de la función map(noise()), se retiraron las partículas muertas del array sp (sistema de partículas) y se crearon dos sistemas de partículas en el sketch

--- classes/Particula.js
+++ classes/Particula.js
@@ -1,3 +1,35 @@
 class Particula {
-  constructor(_x, _y) {}
+  constructor(_x, _y, _color) {
+    this.pos = createVector(_x, _y);
+    this.lifespan = Math.ceil(random(10, 100));
+    this.isAlive = true;
+    this.side = Math.ceil(random(3, 15));
+    this.color = _color;
+    // this.vel = random(-1, 1);
+  }
+  update() {
+    // this.pos += this.vel;
+    this.r = random();
+    if (this.r < 0.25) {
+      this.pos.y -= this.side;
+    } else if (this.r < 0.5) {
+      this.pos.x += this.side;
+    } else if (this.r < 0.75) {
+      this.pos.y += this.side;
+    } else {
+      this.pos.x -= this.side;
+    }
+
+    this.lifespan -= 1;
+    if (this.lifespan <= 0) {
+      this.isAlive = false;
+      rect(this.pos.x, this.pos.y, this.side * 2);
+    }
+  }
+
+  display() {
+    fill(this.color);
+    stroke(255);
+    rect(this.pos.x, this.pos.y, this.side);
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
+// let sp = [];
+let s;
+let d;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  // frameRate(10);
+  // for (let i = 0; i < 10; i++) {
+  //   let p = new Particula(windowWidth / 2, windowHeight / 2);
+  //   ps.push(p);
+  // }
+  s = new Sistema();
+  d = new Sistema();
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  background(0, 15);
+  // for (let i = 0; i < ps.lenght; i++) {
+  //   if (p.isAlive) {
+  //     ps[i].update();
+  //     ps[i].display();
+  //     print("funciona");
+  //   }
+  // }
+  s.update();
+  s.display();
+  d.update();
+  d.display();
 }
